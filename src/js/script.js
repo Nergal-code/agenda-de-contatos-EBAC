c10b66bb256bf69346dfd8f6af01145ef40a8481
Caio Lima <53834100+Nergal-code@users.noreply.github.com>
2023-10-01 00:12:52 -0300

    Create script.js
    Added Script JS

---
+++ src/js/script.js
@@ -0,0 +1,36 @@
+const form = document.querySelector('form');
+const removeContactButtons = document.querySelectorAll('.remove-contact-button');
+let inputName = document.getElementById('inputName');
+let inputPhone = document.getElementById('inputPhone');
+
+let tableRow;
+let tableRows = document.querySelector('tbody');
+let tableRowCount = 0;
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (inputName.value.trim().length === 0 || inputPhone.value.trim().length === 0) {
+        console.log('Digite algo');
+    } else {
+        addTableRow();
+        updateContactTotal();
+        inputName.value = '';
+        inputPhone.value = '';
+    }
+});
+
+function addTableRow() {
+    tableRow = `<tr>`;
+    tableRow += `<td><span>${inputName.value}</span></td>`;
+    tableRow += `<td><span>${inputPhone.value}</span></td>`;
+    tableRow += `</tr>`;
+
+    tableRows.innerHTML += tableRow;
+    tableRowCount++;
+}
+
+
+function updateContactTotal() {
+    contactTotal.innerHTML = tableRowCount;
+}
